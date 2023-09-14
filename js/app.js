15db03e975a3d367660375014e73aa80be63159a
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-09-14 21:48:46 +0500

    my section the end

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const buttonSendText = document.querySelector(".buttonBirdText");
+const buttonSend = document.querySelector(".buttonBird");
+buttonSend.addEventListener("click", change);
+
+function change() {
+  if (buttonSend.classList.contains("actif")) {
+    buttonSendText.innerHTML = "SEND";
+  } else buttonSendText.innerHTML = "SENDING";
+  buttonSend.classList.toggle("actif");
+}
